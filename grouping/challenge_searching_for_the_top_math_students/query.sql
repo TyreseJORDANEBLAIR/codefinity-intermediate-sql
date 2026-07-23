1e789a2b2efee16ffe8cc222ec17e76d1a523ca0
TyreseJORDANEBLAIR <>
2026-07-23 23:11:18 +0000

    Challenge: Searching for the Top Math Students

---
+++ grouping/challenge_searching_for_the_top_math_students/query.sql
@@ -0,0 +1,13 @@
+SELECT student_surname,
+        AVG(grade) AS average_grade
+FROM student_grades
+WHERE subject_name = 'Mathematics'
+  AND student_surname IN (
+    SELECT student_surname
+    FROM student_grades
+    WHERE subject_name = 'Mathematics'
+      AND grade >= 90
+  )
+GROUP BY student_surname
+ORDER BY average_grade DESC
+LIMIT 10;
